7a30fce18c4d1ca03479a2bd552a1923668a65f4
Harry Jordan <harry@inquisitivesoftware.com>
2014-07-22 22:15:52 +0100

    Further swiftifying.. now we're getting somewhere

--- UIView+ConstraintAdditions.swift
+++ UIView+ConstraintAdditions.swift
@@ -14,7 +14,7 @@ extension UIView {
 						horizontalFormat: String? = nil, horizontalOptions: NSLayoutFormatOptions = NSLayoutFormatOptions(0), horizontalPriority: Float? = nil,
 						verticalFormat: String? = nil, verticalOptions: NSLayoutFormatOptions = NSLayoutFormatOptions(0), verticalPriority: Float? = nil) -> [NSLayoutAttribute : NSLayoutConstraint] {
 		
-		func createConstraintsFromFormat(#format: String, #options: NSLayoutFormatOptions, metrics: [String : Double]? = nil, priority: Float = 1000) -> [NSLayoutConstraint] {
+		let createConstraintsFromFormat = {(#format: String, #options: NSLayoutFormatOptions, metrics: [String : Double]?, priority: Float) -> [NSLayoutConstraint] in
 			let constraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options:options, metrics: metrics, views: views) as [NSLayoutConstraint]
 			
 			return constraints.map {constraint in
@@ -28,7 +28,7 @@ extension UIView {
 		
 		if let format = horizontalFormat {
 			var p: Float = horizontalPriority ? horizontalPriority! : priority
-			constraints += createConstraintsFromFormat(format: format, options: horizontalOptions, metrics: metrics, priority: p)
+			constraints += createConstraintsFromFormat(format, horizontalOptions, metrics, p)
 		}
 		
 		if var format: NSString = verticalFormat {
@@ -38,7 +38,7 @@ extension UIView {
 			}
 			
 			var p: Float = verticalPriority ? verticalPriority! : priority
-			constraints += createConstraintsFromFormat(format: format, options: verticalOptions, metrics: metrics, priority:p)
+			constraints += createConstraintsFromFormat(format, verticalOptions, metrics, p)
 		}
 		
 		self.addConstraints(constraints)
@@ -53,58 +53,34 @@ extension UIView {
 	
 	
 	func addConstraints(constraints: [NSLayoutConstraint], priority: UILayoutPriority) {
-		for constraint in constraints {
-			constraint.priority = priority;
-		}
-		
-		self.addConstraints(constraints);
+		self.addConstraints(constraints.map { constraint in
+			constraint.priority = priority
+			return constraint
+		})
 	}
 
 	
 	func fillView(parentView: UIView, priority: UILayoutPriority = 1000) -> [NSLayoutAttribute : NSLayoutConstraint] {
-		var constraints = [NSLayoutAttribute : NSLayoutConstraint]()
-		
-		switch(parentView) {
-			case nil:
-				println("\(self) is trying to add constraints to fill a nil view");
-			
-			case self:
-				println("\(self) trying to add constraints that are self-referential");
-				return Dictionary();
-			
-			default:
-				constraints = parentView.addConstraints(
-					views: ["view" : self],
-					priority: priority,
-					horizontalFormat: "|[view]|",
-					verticalFormat: "|[view]|"
-				)
-		}
-		
-		return constraints
+		return parentView.addConstraints(
+			views: ["view" : self],
+			priority: priority,
+			horizontalFormat: "|[view]|",
+			verticalFormat: "|[view]|"
+		)
 	}
 
 	
 	func centerView(viewToCenter: UIView, relativeToView: UIView, xAxis: Bool = false, yAxis: Bool = false, priority: UILayoutPriority = 1000) -> [NSLayoutAttribute : NSLayoutConstraint] {
+		let center = {(axis: NSLayoutAttribute) -> NSLayoutConstraint in
+			let constraint = NSLayoutConstraint(item:viewToCenter, attribute:axis, relatedBy:NSLayoutRelation.Equal, toItem:relativeToView, attribute:axis, multiplier:1, constant:0)
+			constraint.priority = priority;
+			self.addConstraint(constraint)
+			return constraint
+		}
 		
 		var constraints = [NSLayoutAttribute : NSLayoutConstraint]()
-		
-		if(xAxis) {
-			let constraint = NSLayoutConstraint(item:viewToCenter, attribute:NSLayoutAttribute.CenterX, relatedBy:NSLayoutRelation.Equal, toItem:relativeToView, attribute:NSLayoutAttribute.CenterX, multiplier:1, constant:0)
-			constraint.priority = priority;
-			self.addConstraint(constraint)
-			
-			constraints[NSLayoutAttribute.CenterX] = constraint;
-		}
-		
-		if(yAxis) {
-			let constraint = NSLayoutConstraint(item:viewToCenter, attribute:NSLayoutAttribute.CenterY, relatedBy:NSLayoutRelation.Equal, toItem:relativeToView, attribute:NSLayoutAttribute.CenterY, multiplier:1, constant:0)
-			constraint.priority = priority;
-			self.addConstraint(constraint)
-			
-			constraints[NSLayoutAttribute.CenterY] = constraint;
-		}
-		
+		constraints[.CenterX] = center(.CenterX)
+		constraints[.CenterY] = center(.CenterY)
 		return constraints;
 	}
 
